c9f8d7b58702c5d5829b82c70f0a8ad3728bb19b
소연경 <102569551+soyanggaeng@users.noreply.github.com>
2023-11-10 00:36:54 +0900

    mobile adjustment and list scroll function

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     loadHeader();
     loadFooter();
+    initSwipeFeatures(); // Initialize swipe features after the DOM is loaded
 });
 
 function loadHeader() {
@@ -26,11 +27,56 @@ document.getElementById('menu-icon').addEventListener('click', function() {
     dropdown.classList.toggle('hidden');
 });
 
+// Initialize swipe features and arrow visibility
+function initSwipeFeatures() {
+    const featureScroll = document.querySelector('.feature-scroll');
+    const prevArrow = document.querySelector('.feature-nav.prev');
+    const nextArrow = document.querySelector('.feature-nav.next');
+
+    function checkArrows() {
+        const isScrollable = featureScroll.scrollWidth > featureScroll.clientWidth;
+        prevArrow.style.display = isScrollable ? 'block' : 'none';
+        nextArrow.style.display = isScrollable ? 'block' : 'none';
+    }
+
+    checkArrows(); // Check arrows on page load
+    window.addEventListener('resize', checkArrows); // Check arrows on window resize
+
+    // Add event listeners for arrows
+    prevArrow.addEventListener('click', () => scrollFeatures('prev'));
+    nextArrow.addEventListener('click', () => scrollFeatures('next'));
+
+    // Add event listeners for touch events
+    featureScroll.addEventListener('touchstart', handleTouchStart, false);
+    featureScroll.addEventListener('touchmove', handleTouchMove, false);
+    featureScroll.addEventListener('touchend', handleTouchEnd, false);
+}
+
+// Swipe functionality
+let touchStartX = 0;
+let touchEndX = 0;
+
+function handleTouchStart(e) {
+    touchStartX = e.touches[0].clientX;
+}
+
+function handleTouchMove(e) {
+    touchEndX = e.touches[0].clientX;
+}
+
+function handleTouchEnd() {
+    if (touchStartX - touchEndX > 50) {
+        scrollFeatures('next');
+    } else if (touchStartX - touchEndX < -50) {
+        scrollFeatures('prev');
+    }
+}
+
 function scrollFeatures(direction) {
     var container = document.querySelector('.feature-scroll');
     var containerWidth = container.offsetWidth;
     var item = document.querySelector('#feature-list li');
-    var itemWidth = item ? item.offsetWidth : 0; // Fallback in case there is no item
+    var itemWidth = item ? item.offsetWidth : 0;
 
     if (direction === 'prev') {
         container.scrollLeft -= itemWidth;
@@ -38,4 +84,3 @@ function scrollFeatures(direction) {
         container.scrollLeft += itemWidth;
     }
 }
-
