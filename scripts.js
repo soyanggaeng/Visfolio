d1c3b300a8e07f091390de51679b9efd0a0d3c2b
소연경 <102569551+soyanggaeng@users.noreply.github.com>
2023-11-20 14:22:37 +0900

    news csv and image connect update

--- scripts.js
+++ scripts.js
@@ -26,120 +26,19 @@ async function loadFooter() {
 }
 
 function initializePage() {
-    populateSearchSuggestions();
+    fetchCompanies().then(populateSearchSuggestions);
     setupSearch();
 }
 
-let touchStartX = 0;
-let touchEndX = 0;
-
-function handleTouchStart(e) {
-    touchStartX = e.touches[0].clientX;
-}
-
-function handleTouchMove(e) {
-    touchEndX = e.touches[0].clientX;
-}
-
-function handleTouchEnd() {
-    if (touchStartX - touchEndX > 50) {
-        scrollFeatures('next');
-    } else if (touchStartX - touchEndX < -50) {
-        scrollFeatures('prev');
+async function fetchCompanies() {
+    const response = await fetch('crawling/companies.json');
+    if (!response.ok) {
+        throw new Error('Failed to load company data');
     }
+    return await response.json();
 }
 
-function scrollFeatures(direction) {
-    var container = document.querySelector('.feature-scroll');
-    var containerWidth = container.offsetWidth;
-    var item = document.querySelector('#feature-list li');
-    var itemWidth = item ? item.offsetWidth : 0;
-
-    if (direction === 'prev') {
-        container.scrollLeft -= itemWidth;
-    } else {
-        container.scrollLeft += itemWidth;
-    }
-}
-
-// 더미 데이터
-const companies = [
-    { 
-        name: "AlphaTech", 
-        wordCloud: "wordcloud_alpha.jpg", 
-        news: [
-            { 
-                outlet: "TechCrunch", 
-                time: "2 hours ago", 
-                title: "AlphaTech launches new product", 
-                link: "https://techcrunch.com/alpha-tech-new-product" 
-            },
-            { 
-                outlet: "The Verge", 
-                time: "5 hours ago", 
-                title: "AlphaTech reports record profits", 
-                link: "https://theverge.com/alpha-tech-record-profits" 
-            }
-        ]
-    },
-    { 
-        name: "BetaSolutions", 
-        wordCloud: "wordcloud_beta.jpg", 
-        news: [
-            { 
-                outlet: "Bloomberg", 
-                time: "1 day ago", 
-                title: "BetaSolutions poised to disrupt the market", 
-                link: "https://bloomberg.com/betasolutions-market-disruption" 
-            },
-            { 
-                outlet: "Forbes", 
-                time: "2 days ago", 
-                title: "BetaSolutions CEO on the future of tech innovation", 
-                link: "https://forbes.com/betasolutions-ceo-interview" 
-            }
-        ]
-    },
-    { 
-        name: "GammaCorp", 
-        wordCloud: "wordcloud_gamma.jpg", 
-        news: [
-            { 
-                outlet: "Reuters", 
-                time: "3 hours ago", 
-                title: "GammaCorp's groundbreaking approach to AI", 
-                link: "https://reuters.com/gammacorp-ai-breakthrough" 
-            },
-            { 
-                outlet: "CNBC", 
-                time: "1 hour ago", 
-                title: "GammaCorp stock surges after earnings beat", 
-                link: "https://cnbc.com/gammacorp-earnings-surprise" 
-            }
-        ]
-    },
-    { 
-        name: "DeltaElectronics", 
-        wordCloud: "wordcloud_delta.jpg", 
-        news: [
-            { 
-                outlet: "TechRadar", 
-                time: "2 days ago", 
-                title: "DeltaElectronics announces innovative battery tech", 
-                link: "https://techradar.com/deltaelectronics-battery-tech" 
-            },
-            { 
-                outlet: "Engadget", 
-                time: "3 days ago", 
-                title: "DeltaElectronics to partner with major car manufacturer", 
-                link: "https://engadget.com/deltaelectronics-car-partnership" 
-            }
-        ]
-    },
-    // You can continue adding more companies and news items here
-];
-
-function populateSearchSuggestions() {
+function populateSearchSuggestions(companies) {
     const searchSuggestions = document.getElementById('search-suggestions');
     companies.forEach(company => {
         const option = document.createElement('option');
@@ -158,51 +57,55 @@ function setupSearch() {
 
     searchBtn.addEventListener('click', () => {
         const searchQuery = searchInput.value.toLowerCase();
-        const company = companies.find(c => c.name.toLowerCase() === searchQuery);
-        if (company) {
-            displayWordCloud(company.wordCloud);
-            displayNews(company.news);
-        } else {
-            alert("Company not found");
-        }
+        displayWordCloud(searchQuery);
+        displayNews(searchQuery);
     });
     console.log('Search setup complete');
 }
 
-function displayWordCloud(imageFile) {
+async function displayWordCloud(companyName) {
+    const imageFilePath = `wordcloud/${companyName}_wordcloud.jpg`;
     let image = document.getElementById('wordcloud-image');
-    image.src = imageFile;
+    image.src = imageFilePath;
     image.style.display = 'block';
 }
 
-function displayNews(newsItems) {
-    let newsSection = document.querySelector('.news-section');
-    newsSection.innerHTML = ''; // Clear existing news items
+async function displayNews(companyName) {
+    const csvFilePath = `crawling/${companyName}.csv`;
 
-    newsItems.forEach(item => {
+    const response = await fetch(csvFilePath);
+    if (!response.ok) {
+        console.error('Failed to load news data');
+        return;
+    }
+    const csvText = await response.text();
+
+    parseAndDisplayNews(csvText);
+}
+
+function parseAndDisplayNews(csvText) {
+    let newsSection = document.querySelector('.news-section');
+    newsSection.innerHTML = '';
+
+    const rows = csvText.split('\n');
+    rows.forEach((row, index) => {
+        if (index === 0 || row === '') return;
+
+        const columns = row.split(',');
+        const outlet = columns[0];
+        const time = columns[2];
+        const title = columns[1];
+        const link = columns[3];
+
         let article = document.createElement('article');
         article.className = 'news-item';
 
-        // Create and append the outlet element
-        let outletElem = document.createElement('span');
-        outletElem.className = 'news-outlet';
-        outletElem.textContent = item.outlet + " | ";
-        article.appendChild(outletElem);
-
-        // Create and append the time element
-        let timeElem = document.createElement('span');
-        timeElem.className = 'news-time';
-        timeElem.textContent = item.time + " | ";
-        article.appendChild(timeElem);
-
-        // Create and append the title element as a link
         let titleLink = document.createElement('a');
-        titleLink.href = item.link;
-        titleLink.textContent = item.title;
+        titleLink.href = link;
+        titleLink.textContent = `${outlet} - ${time} - ${title}`;
         titleLink.className = 'news-title';
         article.appendChild(titleLink);
 
-        // Append the article to the news section
         newsSection.appendChild(article);
     });
 }
